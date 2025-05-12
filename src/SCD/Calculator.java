47b7bf238b9ddef958929aea10f2686718ea1d4a
momina-mir-nutech <mominamir@nutech.edu.pk>
2025-05-12 14:57:49 +0500

    Check

--- src/SCD/Calculator.java
+++ src/SCD/Calculator.java
@@ -1,10 +1,24 @@
 package SCD;
 
 public class Calculator {
-
+    int a, b;
+    public int add(int a, int b) {
+	return a+b;
+    }
+    public int div(int a, int b) {
+   	return a/b;
+       }
+    public int mul(int a, int b) {
+   	return a*b;
+       }
+   
     public static void main(String[] args) {
-	// TODO Auto-generated method stub
-
+	
+	Calculator c = new Calculator();
+	System.out.println("Desired Result of addition is " +c.add(2, 4) );
+	System.out.println("Desired Result of division is " +c.div(4, 5) );
+	System.out.println("Desired Result of multiplication is " +c.mul(3, 3) );
+	
     }
 
 }
